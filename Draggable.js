b0ee6b719935936b991208d9eea7443e073bca8f
Ihor Bondarenko <ihor.bondarenko@acropolium.com>
2017-09-15 18:18:51 +0300

    fixed to draggable

--- Draggable.js
+++ Draggable.js
@@ -22,21 +22,48 @@
         };
     }
 
-    (function _addDraggableDirective(){
-        Vue.directive( "aco-vue-draggable", {
-            bind: function(el, binding, vnode){},
-            update: function(e){}
-        });
-    })();
+    //console.log(Modernizr.hasEvent('dragstart'));
 
     function Draggable(el, options) {
+        if (!(el && el.nodeType && el.nodeType === 1)) {
+            throw 'Sortable: `el` must be HTMLElement, and not ' + {}.toString.call(el);
+        }
+        this.el = el;
+        this.el.draggable = true;
+
+        this.el.addEventListener('drag', function(e){
+           // console.log(event);
+        });
+        this.el.addEventListener('drop', function(e){
+            e.preventDefault();
+            console.log('%s   %s', 'drop', e)
+        });
+        this.el.addEventListener('dragover', function(e){
+            e.preventDefault();
+            e.target.style.backgroundColor = 'lightgray';
+            console.log('%s   %s', 'dragover', e)
+        });
+        this.el.addEventListener('dragleave', function(e){
+            e.preventDefault();
+            e.target.style.backgroundColor = '';
+            console.log('%s   %s', 'dragleave', e)
+        });
 
     }
 
     Draggable.create = function (el, options) {
-        return new Sortable(el, options);
+        return new Draggable(el, options);
     };
 
+    (function _addDraggableDirective(){
+        Vue.directive( "aco-vue-draggable", {
+            bind: function(el, binding, vnode){
+                console.log(vnode);
+                Draggable.create(el,{});
+            },
+            update: function(e){}
+        });
+    })();
 
     // Export
     Draggable.version = '0.0.1b';
