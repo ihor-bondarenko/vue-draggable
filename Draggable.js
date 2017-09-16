6e1faf8e8aaaa2367a6cd0cad4d393c6393d9dc3
ihor <guest@localhost.localdomain>
2017-09-16 14:47:05 +0300

    fixed

--- Draggable.js
+++ Draggable.js
@@ -22,7 +22,22 @@
         };
     }
 
-    //console.log(Modernizr.hasEvent('dragstart'));
+    console.log(Modernizr.hasEvent('touchmove'));
+
+    function logEvent(e) {
+      var logContainerEl = document.getElementById('eventLog');
+      if(!logContainerEl) {
+        logContainerEl = document.createElement('div');
+        logContainerEl.setAttribute('id', 'eventLog');
+        document.appendChild(logContainerEl);
+      }
+      var newDiv = document.createElement("div");
+      newDiv.setAttribute('class', 'event-type-container');
+      var elType = document.createElement('span');
+      elType.insertAdjacentHTML('afterbegin', e.type);
+      newDiv.appendChild(elType)
+      logContainerEl.appendChild(newDiv);
+    }
 
     function Draggable(el, options) {
         if (!(el && el.nodeType && el.nodeType === 1)) {
@@ -31,29 +46,50 @@
         this.el = el;
         this.el.draggable = true;
 
+        this.el.addEventListener('mousedown', function(e){
+          console.log(e);
+          logEvent(e);
+        });
+
+        this.el.addEventListener('touchstart', function(e){
+          e.preventDefault();
+          e.stopPropagation();
+        //  logEvent(e);
+        });
+        this.el.addEventListener('pointerdown', function(e){
+        //  e.preventDefault();
+          e.stopPropagation();
+          logEvent(e);
+          var elem = document.elementFromPoint(e.clientX, e.clientY);
+          //console.log(elem);
+        });
+        this.el.addEventListener('touchend', function(e){
+        //  e.preventDefault();
+        //  e.stopPropagation();
+          var changedTouch = e.changedTouches[0];
+          var elem = document.elementFromPoint(changedTouch.clientX, changedTouch.clientY);
+        //  console.log(elem);
+        })
+
         this.el.addEventListener('drag', function(e){
-            e.preventDefault();
-           // console.log(event);
+          //  e.preventDefault();
+          //  console.log(e);
         });
         this.el.addEventListener('dragstart', function(e){
             //e.preventDefault();
-            console.log(e);
             e.dataTransfer.setData("text/plain", e.target.id);
         });
         this.el.addEventListener('drop', function(e){
-            e.preventDefault();
+            //e.preventDefault();
             e.target.style.backgroundColor = 'inherit';
-            console.log('%s   %s', 'drop', e)
         });
         this.el.addEventListener('dragover', function(e){
-            e.preventDefault();
+            //e.preventDefault();
             e.target.style.backgroundColor = 'lightgray';
-            console.log('%s   %s', 'dragover', e)
         });
         this.el.addEventListener('dragleave', function(e){
-            e.preventDefault();
+            //e.preventDefault();
             e.target.style.backgroundColor = 'inherit';
-            console.log('%s   %s', 'dragleave', e)
         });
 
     }
@@ -65,7 +101,6 @@
     (function _addDraggableDirective(){
         Vue.directive( "aco-vue-draggable", {
             bind: function(el, binding, vnode){
-                console.log(vnode);
                 Draggable.create(el,{});
             },
             update: function(e){}
